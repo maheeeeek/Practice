1067b63aac614229556b3a39d4e411e866341c44
maheeeeek <mahek6434@gmail.com>
2026-07-08 23:36:07 +0600

    Update index.js

---
+++ JavaScript/index.js
@@ -0,0 +1,23 @@
+// Rock paper scissor game 
+
+const rPs = (user, computer) => {
+
+    if (user === computer) return "draw";
+
+    if (user === "rock" && computer === "scissor") return "user";
+    if (user === "paper" && computer === "rock") return "user";
+    if (user === "scissor" && computer === "paper") return "user";
+
+    return "computer";
+
+}
+
+
+console.log(rPs("paper","scissor"));
+
+
+//  for loop
+
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
